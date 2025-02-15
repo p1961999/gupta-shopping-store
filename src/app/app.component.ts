deb443c24e7558ddcb58df38ba7122b19a9bb344
Prachi Gupta <prachigupta19699@gmail.com>
2025-02-15 13:36:19 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { NavigationModalComponent } from "./component/navigation-modal/navigation-modal.component";
+import { FooterComponent } from "./component/footer/footer.component";
+import { CartComponent } from "./component/cart/cart.component";
+import { AuthService } from './service/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NavigationModalComponent, FooterComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'gupta-shopping-store';
+
+  constructor(private authService: AuthService){}
+
+  isAuthenticated(): boolean{
+    return this.authService.isAuthenticated();
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,59 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { ProductListComponent } from './component/product/product-list/product-list.component';
+import { ProductDetailsComponent } from './component/product/product-details/product-details.component';
+import { CheckoutComponent } from './component/checkout/checkout.component';
+import { OrdersComponent } from './component/orders/orders.component';
+import { AuthGuard } from '../app/auth.guard';
+import { CartComponent } from './component/cart/cart.component';
 
-export const routes: Routes = [];
+export const APP_ROUTES: Routes = [
+  {
+    path: 'home',
+    redirectTo: '/',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  
+  {
+    path: 'products',
+    component: ProductListComponent, // Show all products
+  },
+  {
+    path: 'products/:id',
+    component: ProductDetailsComponent, // Show product details
+  },
+
+  {
+    path: 'cart',
+    component: CartComponent, // Shopping Cart
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'checkout',
+    component: CheckoutComponent,
+    canActivate: [AuthGuard], // Protected Route - Only Logged-in Users
+  },
+
+  {
+    path: 'orders',
+    component: OrdersComponent,
+    canActivate: [AuthGuard], // Protected Route
+  },
+
+  {
+    path: 'users',
+    loadChildren: () =>
+      import('../app/component/auth/users-routing').then(
+        (m) => m.USERS_ROUTES
+      ),
+  },
+
+  {
+    path: '**',
+    redirectTo: 'home', // Redirect unknown paths to home
+  },
+];
